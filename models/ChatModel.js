1db29b873b15f6c7ec14526b252cc6498f9e9ffc
Noah Wenig <nwenig@trinity.edu>
2025-05-01 17:34:46 -0500

    removed some console logs

--- models/ChatModel.js
+++ models/ChatModel.js
@@ -11,8 +11,6 @@ function sendMessage(senderId, roomId, message){
     //db query here
     const newMsg = {Message_ID: messages.length +1 , Room_ID: roomId, Sender_ID: senderId, Message: message}; 
     messages.push(newMsg); 
-    console.log("new message: " + newMsg); 
-    console.log("all messages: " + message); 
 }
 
   const users = [
